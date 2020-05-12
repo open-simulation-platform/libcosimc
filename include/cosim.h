2ba5c51c26efc0e6cb89ab0d36c5f0182bfd3485
Levi <levi.jamt@dnvgl.com>
2020-05-12 13:50:44 +0200

    adding MPL 2.0 headers

--- include/cosim.h
+++ include/cosim.h
@@ -1,6 +1,10 @@
 /**
  *  \file
  *  Main header file
+ *  \copyright
+ *      This Source Code Form is subject to the terms of the Mozilla Public
+ *      License, v. 2.0. If a copy of the MPL was not distributed with this
+ *      file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 #ifndef COSIM_H
 #define COSIM_H
