f8458ce9079467f1eafc2e67de273a80ce0010a3
Levi <levi.jamt@dnvgl.com>
2020-05-29 12:40:52 +0200

    depending on libcosim v0.7.0

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class LibCosimCConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "virtualrunenv"
     requires = (
-        "libcosim/0.7.0@osp/master"
+        "libcosim/0.7.0@osp/v0.7.0"
         )
 
     def set_version(self):
