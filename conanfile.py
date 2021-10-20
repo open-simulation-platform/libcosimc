9706fa3f47aa0356777b50e8680d420793eb720f
Levi Jamt <levi.jamt@dnvgl.com>
2021-10-20 10:53:25 +0200

    preparing for release (#30)

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class LibCosimCConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "virtualrunenv"
     requires = (
-        "libcosim/0.8.0@osp/testing"
+        "libcosim/0.8.2@osp/testing"
         )
 
     def set_version(self):
