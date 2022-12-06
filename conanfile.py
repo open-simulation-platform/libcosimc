148d1d16fdc8b7ea3de16e56ba2d0a510e0e18c8
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2022-12-06 18:14:56 +0100

    ok..

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class LibCosimCConan(ConanFile):
         "revision": "auto"
     }
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_find_package", "virtualrunenv"
+    generators = "cmake", "cmake_find_package", "virtualrunenv"
     requires = (
         "libcosim/0.10.0@osp/stable"
         )
