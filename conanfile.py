78722864ee5409c4b015f4b336c0d244a4fcf5fe
Roger Eivind Stenbro <Roger.Eivind.Stenbro@dnv.com>
2022-12-06 13:12:27 +0100

    preparing for release

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class LibCosimCConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "virtualrunenv"
     requires = (
-        "libcosim/0.10.0@osp/testing"
+        "libcosim/0.10.0@osp/stable"
         )
 
     def set_version(self):
