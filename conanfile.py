0398d2cfb61fb1f78270628e388cefb6657613d0
Roger Eivind Stenbro <Roger.Eivind.Stenbro@dnv.com>
2024-12-12 17:15:35 +0100

    also bump libcosim version

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class LibCosimCConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "virtualrunenv"
     requires = (
-        "libcosim/0.10.2@osp/stable"
+        "libcosim/0.10.4@osp/stable"
         )
 
     def set_version(self):
